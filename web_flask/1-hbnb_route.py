87e66888086c0ecd4589383db4f4472e8b47b06d
asterisksmoses <asterisksmoses@gmail.com>
2024-07-28 10:29:51 +0000

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,10 +5,15 @@ application = Flask(__name__)
 """After importing the flask module an application instance of
 Flask was created."""
 
-application@route('/', strict_slashes=False)
+@application.route('/', strict_slashes=False)
 def home():
     """This function displays Hello HBNB!"""
     return "Hello HBNB!"
+
+@application.route('/hbnb', strict_slashes=False)
+def flask_hbnb():
+    """This function displays HBNB!"""
+    return "HBNB"
 
 if __name__ == "__main__":
    application.run(host='0.0.0.0', port=5000)
