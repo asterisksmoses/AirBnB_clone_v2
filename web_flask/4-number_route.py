47bbf8babca435b7f9a9d3a21666c580f1706280
asterisksmoses <asterisksmoses@gmail.com>
2024-07-28 12:45:49 +0000

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""After importing the flask module an application instance of
+Flask was created."""
+
+from flask import Flask
+application = Flask(__name__)
+application.url_map.strict_slashes = False
+
+@application.route('/', strict_slashes=False)
+def home():
+    """This function displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+@application.route('/hbnb', strict_slashes=False)
+def flask_hbnb():
+    """This function displays HBNB!"""
+    return "HBNB"
+
+@application.route('/c/<text>')
+def text_params(text):
+    """This function runs the test parts with any parameters."""
+     
+    text_w_no_underscore = text.replace('_', ' ')
+    return "C {}".format(text_w_no_underscore)
+
+@application.route('/python', defaults={'text': 'is_cool'})
+@application.route('/python/<text>')
+def py_w_text_parameters(text):
+    """This function prints the defualt value is_cool."""
+    text_w_no_underscore = text.replace('_', ' ')
+    return "Python {}".format(text_w_no_underscore)
+
+@application.route('/number/<int:n>')
+def fl_number(n):
+    """This function ensures that a number is passed as a
+parameter."""
+    return "{} is a number".format(n)
+
+if __name__ == "__main__":
+   application.run(host='0.0.0.0', port=5000)
