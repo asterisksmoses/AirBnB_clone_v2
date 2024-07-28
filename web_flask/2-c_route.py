6bbbe1d010c32c09708ddd3b88dc5e831781451d
asterisksmoses <asterisksmoses@gmail.com>
2024-07-28 12:11:44 +0000

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""After importing the flask module an application instance of
+Flask was created."""
+
+from flask import Flask
+application = Flask(__name__)
+application.url_map.strict_slashes = False
+
+@application.route('/', strict_slashes=False)
+def home():
+    """This function displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+@application.route('/hbnb', strict_slashes=False)
+def flask_hbnb():
+    """This function displays HBNB!"""
+    return "HBNB"
+
+@application.route('/c/<text>')
+def text_params(text):
+    """This function runs the test parts with any parameters."""
+     
+    text_w_no_underscore = text.replace('_', ' ')
+    return "C {}".format(text_w_no_underscore)
+
+if __name__ == "__main__":
+   application.run(host='0.0.0.0', port=5000)
